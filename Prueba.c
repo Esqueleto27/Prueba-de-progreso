3adb72716bd4a200980d4207d14cfe32e3fa6123
Matheo Flores <105876330+Esqueleto27@users.noreply.github.com>
2023-05-02 12:48:21 -0500

    Ponemos la estructura para alamacenar datos.

--- Prueba.c
+++ Prueba.c
@@ -3,7 +3,13 @@
 int main() {
     int num1, num2, num3, num4, num5, num6, num7, count;
     
-    printf("Ingrese 7 números enteros: ");
+    printf("Ingrese 7 números enteros separados por un espacio: ");
     scanf("%d %d %d %d %d %d %d", &num1, &num2, &num3, &num4, &num5, &num6, &num7);
+    count = 0;
+    if (num1 != 0) count++;
+    if (num2 != 0) count++;
+    if (num3 != 0) count++;
+    if (num4 != 0) count++;
+    if (num5 != 0) count++;
     
 }
