98f475d5442bf5426b7e74d81f45c6c7b2fcc36a
Matheo Flores <105876330+Esqueleto27@users.noreply.github.com>
2023-05-02 12:43:24 -0500

    Declaracion de variables y entrada y salida de datos

---
+++ Prueba.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, num3, num4, num5, num6, num7, count;
+    
+    printf("Ingrese 7 números enteros: ");
+    scanf("%d %d %d %d %d %d %d", &num1, &num2, &num3, &num4, &num5, &num6, &num7);
+    
+}
